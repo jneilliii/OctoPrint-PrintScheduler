fe242099b70a2de8dc56aec00d28d2d83da7da60
jneilliii <jneilliii+github@gmail.com>
2022-10-15 16:49:43 -0400

    1.0.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ plugin_package = "octoprint_printscheduler"
 plugin_name = "Print Scheduler"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "0.0.5rc3"
+plugin_version = "1.0.0"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
